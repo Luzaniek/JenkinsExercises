1ef578e177851b5b70373fe75260ae8ea75e029e
Piotr Luzny <piotr.luzny@delaval.com>
2020-02-28 16:18:27 +0100

    1.2

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 javac src/main/java/PageStart.java
-jar cfme main.jar manifest.txt PageStart src/main/java/PageStart.class
+jar cfm main.jar manifest.txt PageStart src/main/java/PageStart.class
 java -jar main.jar
